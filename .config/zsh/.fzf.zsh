a8c01c58ba127633984f3359941d7cc37958b705
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-05-12 00:19:25 +0300

    Move fzf scripts to zsh config dir

---
+++ .config/zsh/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == *$HOME/.config/zsh/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.config/zsh/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$HOME/.config/zsh/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$HOME/.config/zsh/fzf/shell/key-bindings.zsh"
