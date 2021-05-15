eda80c35d6fa17bb632f038b1fb071499ee230b4
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-05-15 18:59:09 +0300

    Add: Elcord-mode (Discord RPC) to GNU Emacs config

--- .config/reomacs/init.el
+++ .config/reomacs/init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-superstar hydra rainbow-mode beacon beacon-mode electric-spacing electric-operator electric-cursor electric-case undo-tree all-the-icons general evil-tutor evil-collection evil use-package)))
+   '(elcord org-superstar hydra rainbow-mode beacon beacon-mode electric-spacing electric-operator electric-cursor electric-case undo-tree all-the-icons general evil-tutor evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
