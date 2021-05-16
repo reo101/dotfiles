192d7a941649647c50fbd13eb087253426182a78
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-05-16 11:14:35 +0300

    Add: Wakatime-mode with .secrets.eld file for secret vars

--- .config/reomacs/init.el
+++ .config/reomacs/init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-anzu evil-snipe evil-numbers evil-goggles elcord org-superstar hydra rainbow-mode beacon beacon-mode electric-spacing electric-operator electric-cursor electric-case undo-tree all-the-icons general evil-tutor evil-collection evil use-package)))
+   '(wakatime-mode evil-anzu evil-snipe evil-numbers evil-goggles elcord org-superstar hydra rainbow-mode beacon beacon-mode electric-spacing electric-operator electric-cursor electric-case undo-tree all-the-icons general evil-tutor evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
