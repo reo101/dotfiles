adc279eccb5a8ab88a5d31aec8059de7591817bd
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-05-16 10:40:21 +0300

    Add: Lots of config to GNU Emacs config...
    A few more Evil plugins:
    evil-goggles, evil-snipe

--- .config/reomacs/init.el
+++ .config/reomacs/init.el
@@ -9,10 +9,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elcord org-superstar hydra rainbow-mode beacon beacon-mode electric-spacing electric-operator electric-cursor electric-case undo-tree all-the-icons general evil-tutor evil-collection evil use-package)))
+   '(evil-anzu evil-snipe evil-numbers evil-goggles elcord org-superstar hydra rainbow-mode beacon beacon-mode electric-spacing electric-operator electric-cursor electric-case undo-tree all-the-icons general evil-tutor evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
